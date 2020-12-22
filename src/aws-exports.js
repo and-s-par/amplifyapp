15c1c273e3bc33d54c9c9697d0b4173cad8b007e
Andreas Paredi <andreas.paredi@ggaweb.ch>
2020-12-22 15:03:56 +0100

    Added storage and can now display an image.

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://qshc5gu26betzgh7idkvt6gqwe.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-trrshrmfi5e6xbnpxlafpeoqka"
+    "aws_appsync_apiKey": "da2-trrshrmfi5e6xbnpxlafpeoqka",
+    "aws_user_files_s3_bucket": "notesimages130547-dev",
+    "aws_user_files_s3_bucket_region": "eu-central-1"
 };
 
 
