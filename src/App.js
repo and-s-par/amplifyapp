2ae88a6bc57c23ebbc5b1b00b7b80e20b28141c8
Andreas Paredi <andreas.paredi@ggaweb.ch>
2020-12-23 12:29:27 +0100

    Added first ant design elements (Input, Upload, Button)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+/*import './App.css';*/
+import 'antd/dist/antd.css';
 import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { Button, Input, message, Upload } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 
+const props = {
+  name: 'file',
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
 
 const initialFormState = { name: '', description: '' }
 
@@ -27,7 +47,7 @@ function App() {
     return note;
   }))
   setNotes(apiData.data.listNotes.items);
-}
+  }
 
   async function createNote() {
   if (!formData.name || !formData.description) return;
@@ -38,7 +58,7 @@ function App() {
   }
   setNotes([ ...notes, formData ]);
   setFormData(initialFormState);
-}
+  }
 
   async function deleteNote({ id }) {
     const newNotesArray = notes.filter(note => note.id !== id);
@@ -52,40 +72,42 @@ function App() {
   setFormData({ ...formData, image: file.name });
   await Storage.put(file.name, file);
   fetchNotes();
-}
+  }
 
   return (
     <div className="App">
       <h1>My Notes App</h1>
-      <input
+      <Input
+        placeholder ="Note Name"
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
         value={formData.name}
       />
-      <input
+      <Input
+        placeholder ="Note Description"
         onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
         value={formData.description}
       />
-      <input
-        type="file"
-        onChange={onChange}
-      />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
+      <Upload {...props}>
+        <Button icon={<UploadOutlined />}>Click to Upload</Button>
+      </Upload>
+      <Button onClick={createNote}>Create Note</Button>
+      <p></p>
+      <p></p>
+      <table style={{marginBottom: 30}}>
         {
           notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
+            <tr>
+              <th>{note.id}</th>
+              <th>{note.name}</th>
+              <th>{note.description}</th>
               {
-                  note.image && <img src={note.image} style={{width: 400}} />
+                  <th><img src={note.image} alt="Room for more..." style={{width: 200}} /></th>
               }
-      </div>
-  ))
-}
-      </div>
+              <th><Button onClick={() => deleteNote(note)}>Delete note</Button></th>
+            </tr>
+          ))
+        }
+      </table>
       <AmplifySignOut />
     </div>
   );
